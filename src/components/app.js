9069795a830f2179f7b8ae80c32034d7ce0dfb16
serginij <kotlizkiy@gmail.com>
2019-10-07 20:43:14 +0300

    feat: add mindmap skeleton

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,67 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export const App = () => <h1>Hello world</h1>
+import { Node } from '@components/node'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nodes: [
+        {
+          id: 0,
+          x: 0,
+          y: 0
+        }
+      ],
+      counter: 1
+    }
+  }
+
+  render() {
+    const handleAddNode = () => {
+      this.setState(
+        prevState => ({
+          nodes: [...prevState.nodes, { id: prevState.counter, x: 0, y: 0 }],
+          counter: prevState.counter + 1
+        }),
+        () => console.log(this.state.nodes)
+      )
+    }
+
+    const handleMoveNode = (id, coords) => {
+      let newNodes = this.state.nodes
+      let index = newNodes.indexOf(newNodes.find(el => el.id === id))
+      newNodes[index].x = coords.x
+      newNodes[index].y = coords.y
+
+      // console.log(index)
+      this.setState(
+        {
+          nodes: newNodes
+        },
+        () => console.log(this.state.nodes)
+      )
+      // console.log(id, coords)
+    }
+
+    let map = this.state.nodes.map(el => (
+      <Node
+        onClick={handleAddNode}
+        onMove={handleMoveNode}
+        key={el.id}
+        id={el.id}
+      />
+    ))
+
+    return (
+      <div>
+        <h1>Hello world</h1>
+        {map}
+        {/* <Node onClick={handleAddNode} onMove={handleMoveNode} id={0} /> */}
+        {/* <button onClick={handleAddNode}>Add node</button> */}
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/node/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+import { styled } from 'linaria/react'
+
+export const Node = props => {
+  const [x, setX] = useState(50)
+  const [y, setY] = useState(50)
+  const [dragging, setDragging] = useState(false)
+
+  const handleDrop = event => {
+    event.preventDefault()
+    // console.log(event.clientX)
+    setDragging(false)
+    props.onMove(props.id, { x: x, y: y })
+  }
+
+  const handleOnDrag = event => {
+    event.preventDefault()
+    setX(event.clientX - 20)
+    setY(event.clientY - 25)
+  }
+
+  const handleDragStart = () => {
+    // const falseImg = new Image()
+    // event.dataTransfer.setDragImage(falseImg, x + 50, y + 25)
+    setDragging(true)
+    // console.log(dragging)
+  }
+
+  return (
+    <Container
+      onClick={props.onClick}
+      x={x}
+      y={y}
+      draggable
+      onDragOver={handleOnDrag}
+      onDrop={handleDrop}
+      onDragStart={handleDragStart}
+      dragging={dragging}
+    >
+      Simple node
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  background-color: lightgreen;
+  width: 50px;
+  height: 50px;
+  position: absolute;
+  left: ${props => props.x + 'px'};
+  top: ${props => props.y + 'px'};
+  /* opacity: ${props => (props.dragging ? '0.01' : '1')}; */
+  cursor: grab;
+`
