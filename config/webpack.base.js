07cc58c02728eb844f5df65dedba7f0dec2ede59
serginij <kotlizkiy@gmail.com>
2020-02-11 21:02:54 +0300

    fix: try to fix prod bug

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -9,7 +9,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '../public')
+    path: path.resolve(__dirname, '../public'),
+    publicPath: '/'
   },
   module: {
     rules: [
